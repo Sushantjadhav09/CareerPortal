22d3c069317b025f351ecb2dadd873c248e69ec7
Sushant Jadhav <jadhavsushant568@gmail.com>
2025-12-21 18:40:47 +0530

    applicant api uupdated

---
+++ Backend/CareerPT/src/main/java/com/example/CareerPT/repository/ApplicantRepository.java
@@ -0,0 +1,9 @@
+package com.example.CareerPT.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.CareerPT.entity.Applicant;
+
+public interface ApplicantRepository extends JpaRepository<Applicant, Long>{
+
+}
---
+++ Backend/CareerPT/src/main/java/com/example/CareerPT/service/ApplicantService.java
@@ -0,0 +1,22 @@
+package com.example.CareerPT.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.CareerPT.dto.ApplicantDTO;
+import com.example.CareerPT.entity.Applicant;
+import com.example.CareerPT.repository.ApplicantRepository;
+
+@Service
+public class ApplicantService {
+	
+	@Autowired
+	ApplicantRepository applicantRepository;
+
+	public List<Applicant> getAllapplicant() {
+		return applicantRepository.findAll();
+	}
+
+}
