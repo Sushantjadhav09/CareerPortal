932a8b37ba16c03cddf09b5cfb53f84f6c7b8ca4
Sushant Jadhav <jadhavsushant568@gmail.com>
2025-12-26 18:22:39 +0530

    jwt frontend integration

---
+++ Frontend/career-portal/src/Services/AuthService.tsx
@@ -0,0 +1,10 @@
+ import axios from "axios"
+const base_url = "http://localhost:9295/users/"
+ 
+ const loginUser = async (login:any)=>{
+    return axios.post(`${base_url}login`,login)
+    .then(res=>res.data)
+    .catch(error=>{throw error;})
+ }
+
+ export{loginUser}
